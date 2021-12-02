9fa4af9d3ad52f7e315b0394fa875b0bab0740e7
Moro Owusu Afriyie <ceremoinc99@gmail.com>
2021-12-02 17:11:05 -0100

    fetch the data using d3.json

--- Visualize Data with a Bar Chart/script.js
+++ Visualize Data with a Bar Chart/script.js
@@ -1,1 +1,11 @@
-console.log(d3);
+const datasetURL =
+  "https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/GDP-data.json";
+
+const width = 800;
+const height = 600;
+
+d3.json(
+  "https://raw.githubusercontent.com/FreeCodeCamp/ProjectReferenceData/master/GDP-data.json"
+).then((data) => {
+  console.log(data.data);
+});
