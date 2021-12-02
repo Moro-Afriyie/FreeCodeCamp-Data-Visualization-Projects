e69e5b1045418b3a615e9ce2acf65518bdab7df8
Moro Owusu Afriyie <ceremoinc99@gmail.com>
2021-12-02 17:21:47 -0100

    set up the various variables

--- Visualize Data with a Bar Chart/script.js
+++ Visualize Data with a Bar Chart/script.js
@@ -1,11 +1,27 @@
 const datasetURL =
   "https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/GDP-data.json";
 
+let data = [];
 const width = 800;
 const height = 600;
+const svg = d3.select("svg"); // selects the svg tag
+
+// set the width and height of the svg
+const drawCanvas = () => {
+  svg.attr("width", width);
+  svg.attr("height", height);
+};
+
+const generateScales = () => {};
+
+// bars and tooltips will be drawn here
+const drawBars = () => {};
+
+const generateAxis = () => {};
 
 d3.json(
   "https://raw.githubusercontent.com/FreeCodeCamp/ProjectReferenceData/master/GDP-data.json"
-).then((data) => {
-  console.log(data.data);
+).then((res) => {
+  data = res.data;
+  console.log("data: ", data);
 });
