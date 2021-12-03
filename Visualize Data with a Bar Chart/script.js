6945b24710b585902b7970553e445b516212cf5d
Moro Owusu Afriyie <ceremoinc99@gmail.com>
2021-12-03 10:30:58 -0100

    create the axis for the bar chart

--- Visualize Data with a Bar Chart/script.js
+++ Visualize Data with a Bar Chart/script.js
@@ -4,7 +4,12 @@ const datasetURL =
 let data = [];
 const width = 800;
 const height = 600;
+const padding = 20;
 const svg = d3.select("svg"); // selects the svg tag
+let heightScale;
+let xScale;
+let xAxisScale;
+let yAxisScale;
 
 // set the width and height of the svg
 const drawCanvas = () => {
@@ -12,12 +17,50 @@ const drawCanvas = () => {
   svg.attr("height", height);
 };
 
-const generateScales = () => {};
+const generateScales = () => {
+  heightScale = d3
+    .scaleLinear()
+    .domain([
+      0,
+      d3.max(data, (item) => {
+        return item[1];
+      }),
+    ])
+    .range([0, height - 2 * padding]);
+
+  // to be able to position the bars along the x-axis
+  xScale = d3
+    .scaleLinear()
+    .domain([0, data.length - 1])
+    .range([padding, width - padding]);
+
+  const datesArray = data.map((item) => {
+    return new Date(item[0]);
+  });
+
+  xAxisScale = d3
+    .scaleTime()
+    .domain([d3.min(datesArray), d3.max(datesArray)])
+    .range([padding, width - padding]);
+
+  yAxisScale = d3
+    .scaleLinear()
+    .domain([
+      0,
+      d3.max(data, (item) => {
+        return item[1];
+      }),
+    ])
+    .range([height - padding, padding]);
+};
 
 // bars and tooltips will be drawn here
 const drawBars = () => {};
 
-const generateAxis = () => {};
+const generateAxis = () => {
+  const xAxis = d3.axisBottom(xAxisScale);
+  svg.append("g").call(xAxis).attr("id", "x-axis");
+};
 
 d3.json(
   "https://raw.githubusercontent.com/FreeCodeCamp/ProjectReferenceData/master/GDP-data.json"
